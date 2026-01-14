9d2ccd4c8a43b6d99d60a3643eaa827337372f32
aarchii793 <prajapatiaarchi15@gmail.com>
2026-01-14 15:06:39 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -7,27 +7,36 @@ import cors from 'cors'
 import cookieParser from "cookie-parser"
 import userRouter from "./routes/user.routes.js"
 
-
+import path from "path";
+const __dirname = path.resolve();
 
  const app= express()
  app.use(cors({
    origin:"http://localhost:5173",
    credentials:true
  }))
+ connectdb()
  const port = process.env.PORT || 5000
  app.use(express.json())
  app.use(cookieParser())
  app.use("/api/auth",authRouter)
  app.use("/api/user",userRouter)
 
- 
+ if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
+}
 
 //  app.get("/",(req , res) => {
 //     res.send("working")
 //  })
 
  app.listen(port,()=> {
-    connectdb()
+    
     console.log("server started");
     
  })
